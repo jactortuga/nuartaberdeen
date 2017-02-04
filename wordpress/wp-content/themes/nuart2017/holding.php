65c18ca68ca0879b1fd7dca67a09d57c09abebfd
jactortuga <jactortuga@users.noreply.github.com>
2017-02-04 11:36:09 +0000

    fort imported, custom holding page set up

---
+++ wordpress/wp-content/themes/nuart2017/holding.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Template Name: Holding
+*/
+
+/**
+ * The custom holding page template
+ *
+ *
+ * @package WordPress
+ * @subpackage Tortuga Labs - Nuart2017
+ * @since Tortuga Labs - Nuart2017 1.0
+ */
+
+
+get_header(); ?>
+
+<h1 class="something">STATIC PAGE STOCAZZ</h1>
+
+<?php get_footer(); ?>
