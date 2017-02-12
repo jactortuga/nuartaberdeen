2b4c7ed2eb0331424bbcf195b4fd7ae460944256
jactortuga <jactortuga@users.noreply.github.com>
2017-02-12 14:38:02 +0000

    plain text module styled with subtitle and artists repeater implemented

--- wordpress/wp-content/themes/nuart2017/partials/artists_module.php
+++ wordpress/wp-content/themes/nuart2017/partials/artists_module.php
@@ -13,19 +13,24 @@ $module_artists = get_posts($args);
 
 ?>
 
-<section>
 
-    <? foreach($module_artists as $module_artist):
-        $module_artist_id       = $module_artist->ID;
-        $module_artist_name     = get_the_title($module_artist_id);
-        $module_artist_image    = get_post_thumbnail_id($module_artist_id);
-        $module_artist_link     = get_the_permalink($module_artist_id);
-    ?>
-
-        <h1><?= $module_artist_name ?></h1>
-        <a href="<?= $module_artist_link ?>"><?= $module_artist_link ?></a>
-        <?= wp_get_attachment_image($module_artist_image, 'full'); ?>
-    
-    <? endforeach; ?>
-
-</section>
+<div class="module">
+    <h1 class="module__title module__title--single">Artists</h1>
+    <section class="module__repeater module__repeater--artists">
+        <? foreach($module_artists as $module_artist):
+            $module_artist_id       = $module_artist->ID;
+            $module_artist_name     = get_the_title($module_artist_id);
+            $module_artist_image    = get_post_thumbnail_id($module_artist_id);
+            $module_artist_link     = get_the_permalink($module_artist_id);
+        ?>
+            <div class="module__repeater-item module__repeater-item--artist">
+                <?= wp_get_attachment_image($module_artist_image, 'full', false, array('class' => 'module__repeater-item-image module__repeater-item-image--artist')); ?>
+                <article class="module__repeater-item-content module__repeater-item-content--artist">
+                    <a href="<?= $module_artist_link ?>" class="module__repeater-item-link">
+                        <h1 class="module__repeater-item-title"><?= $module_artist_name ?></h1>
+                    </a>
+                </article>
+            </div>
+        <? endforeach; ?>
+    </section>
+</div>
--- wordpress/wp-content/themes/nuart2017/partials/text_module_plain.php
+++ wordpress/wp-content/themes/nuart2017/partials/text_module_plain.php
@@ -1,11 +1,19 @@
 <?php
 
-$module_content = get_sub_field('module_content');
+$module_subtitle    = (get_sub_field('module_subtitle') ? get_sub_field('module_subtitle') : false);
+$module_content     = get_sub_field('module_content');
 
 ?>
 
-<section>
-    
-    <div><?= $module_content ?></div>
-
-</section>
+<div class="module">
+    <article class="module__article module__article--centered">
+        <? if($module_subtitle):?>
+            <h1 class="module__article-title module__article-title--padding">
+                <?= $module_subtitle ?>
+            </h1>
+        <? endif; ?>
+        <div class="module__text-wrapper module__text-wrapper--left">
+            <?= $module_content ?>
+        </div>
+    </article>
+</div>
