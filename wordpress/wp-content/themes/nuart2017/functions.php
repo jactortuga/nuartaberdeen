3a42956c9a8fab62b763dc6b8f3553baeac92745
jactortuga <jactortuga@users.noreply.github.com>
2017-02-18 14:53:06 +0000

    more posts repeater styling

--- wordpress/wp-content/themes/nuart2017/functions.php
+++ wordpress/wp-content/themes/nuart2017/functions.php
@@ -99,7 +99,7 @@ function tl_scripts() {
 
 	if(!is_admin())
     {
-    	$ver = '18022017A';
+    	$ver = '18022017B';
 
     	wp_enqueue_style('tl-style', get_template_directory_uri() . '/assets/css/main.css', false, $ver);
 
