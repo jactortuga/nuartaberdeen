d39f54a4c0dd3648c427f5a2be64ac3d9d933e4e
jactortuga <jactortuga@users.noreply.github.com>
2017-02-18 17:45:58 +0000

    more fixing

--- wordpress/wp-content/themes/nuart2017/partials/text_module_special.php
+++ wordpress/wp-content/themes/nuart2017/partials/text_module_special.php
@@ -1,6 +1,6 @@
 <?php
 
-$module_title_one   = get_sub_field('module_title_one');
+$module_title       = get_sub_field('module_title');
 $module_content_one = get_sub_field('module_content_one');
 $module_content_two = get_sub_field('module_content_two');
 $module_class       = '--special';
@@ -8,7 +8,7 @@ $module_class       = '--special';
 ?>
 
 <div class="module">
-    <h1 class="module__title module__title--single"><?= $module_title_one ?></h1>
+    <h1 class="module__title module__title--single"><?= $module_title ?></h1>
     <section class="module__double-article">
         <div class="module__article-container module__article-container<?= $module_class ?>">
             <article class="module__article module__article--centered">
