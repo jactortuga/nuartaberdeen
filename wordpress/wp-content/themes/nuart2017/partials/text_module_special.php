faf966bcdc2be614a7ee3651f659fdd91a927b25
jactortuga <jactortuga@users.noreply.github.com>
2017-02-18 17:09:57 +0000

    double and special text modules implemented

--- wordpress/wp-content/themes/nuart2017/partials/text_module_special.php
+++ wordpress/wp-content/themes/nuart2017/partials/text_module_special.php
@@ -2,16 +2,15 @@
 
 $module_title_one   = get_sub_field('module_title_one');
 $module_content_one = get_sub_field('module_content_one');
-$module_title_two   = (get_sub_field('module_title_two') ? get_sub_field('module_title_two') : false);
 $module_content_two = get_sub_field('module_content_two');
-$module_class       = '--double';
+$module_class       = '--special';
 
 ?>
 
 <div class="module">
+    <h1 class="module__title module__title--single"><?= $module_title_one ?></h1>
     <section class="module__double-article">
         <div class="module__article-container module__article-container<?= $module_class ?>">
-            <h1 class="module__title module__title--single"><?= $module_title_one ?></h1>
             <article class="module__article module__article--centered">
                 <div class="module__text-wrapper module__text-wrapper--left">
                     <?= $module_content_one ?>
@@ -19,7 +18,6 @@ $module_class       = '--double';
             </article>
         </div>
         <div class="module__article-container module__article-container<?= $module_class ?>">
-            <h1 class="module__title module__title--single"><?= $module_title_two ?></h1>
             <article class="module__article module__article--centered">
                 <div class="module__text-wrapper module__text-wrapper--left">
                     <?= $module_content_two ?>
